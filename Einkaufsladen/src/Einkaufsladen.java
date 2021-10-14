a093acdf8f3bf14ecef076ce516e967ccb2502b5
IndianaGoof <markus-menke@gmx.net>
2021-10-14 21:23:44 +0200

    Create Einkaufsladen.java
    New

---
+++ Einkaufsladen/src/Einkaufsladen.java
@@ -0,0 +1,7 @@
+package JavaGit.Einkaufsladen.src;
+import javax.swing.*;
+public class Einkaufsladen{
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
